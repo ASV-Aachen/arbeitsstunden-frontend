768093d1ea1ad57b8aadb0966ba53a408384aab7
Ralf Bettermann <mail@ralf-bettermann.de>
2018-06-30 17:43:43 +0200

    removed unused imports

--- src/components/takel/users/TakelList.jsx
+++ src/components/takel/users/TakelList.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import SearchIcon from '@material-ui/icons/Search';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-
 import MembersTable from './MembersTable.jsx';
 
 export default class TakelList extends Component {
