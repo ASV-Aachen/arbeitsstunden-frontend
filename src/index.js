8b874a3085f1200a1b5adfd73e4169cc6279b47e
Christian Baltzer <35217578+Darker97@users.noreply.github.com>
2022-01-04 14:40:54 +0100

    added keycloak

--- src/index.js
+++ src/index.js
@@ -3,6 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Keycloak from 'keycloak-js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+//keycloak init options
+let initOptions = {
+    url:            process.env.REACT_APP_KEYCLOAK_URL, 
+    realm:          process.env.REACT_APP_KEYCLOAK_REALM, 
+    clientId:       process.env.REACT_APP_KEYCLOAK_CLIENTID, 
+    onLoad:         'login-required'
+}
+
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+
+    if (!auth) {
+        window.location.reload();
+    } else {
+        console.info("Authenticated");
+    }
+
+    //React Render
+    ReactDOM.render(<App />, document.getElementById('root'));
+    registerServiceWorker();
+
+    localStorage.setItem("react-token", keycloak.token);
+    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+
+    setTimeout(() => {
+        keycloak.updateToken(70).success((refreshed) => {
+            if (refreshed) {
+                console.debug('Token refreshed' + refreshed);
+            } else {
+                console.warn('Token not refreshed, valid for '
+                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+            }
+        }).error(() => {
+            console.error('Failed to refresh token');
+        });
+
+
+    }, 60000)
+
+}).error(() => {
+    console.error("Authenticated Failed");
+});
